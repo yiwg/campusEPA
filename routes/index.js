36f84d9b6a6781496b9ff43c9893752394d81821
yiwg <yiweiguo2010@126.com>
2017-07-13 11:50:14 +0800

    2221111

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,13 @@ var log4js = require('log4js');
 var logger = log4js.getLogger('normal');
 logger.setLevel('DEBUG');
 /* GET home page. */
-router.get('/portal', function(req, res, next) {
-  logger.debug("进入首页......")
-  res.render('index', { title: 'Express' });
+router.get('/portal2', function(req, res, next) {
+  logger.info("进入首页......")
+  res.render('index-2', { title: 'Express' });
+});
+router.get('/portal2', function(req, res, next) {
+  logger.info("进入首页2......")
+  res.render('index-2', { title: 'Express' });
 });
 
 module.exports = router;
