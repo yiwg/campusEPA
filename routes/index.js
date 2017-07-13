15b7020c4e1784af91edf8a0830a8275091729d4
yiwg <yiweiguo2010@126.com>
2017-07-13 17:47:31 +0800

    2221111

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,23 @@ logger.setLevel('DEBUG');
 /* GET home page. */
 router.get('/portal2', function(req, res, next) {
   logger.info("进入首页......")
-  res.render('index-2', { title: 'Express' });
+  res.render('index2', { title: 'Express' });
 });
-router.get('/portal2', function(req, res, next) {
-  logger.info("进入首页2......")
-  res.render('index-2', { title: 'Express' });
+router.get('/iscroll', function(req, res, next) {
+  logger.info("进入iscroll......")
+  res.render('iscroll', { title: 'Express' });
 });
-
+router.get('/portal5', function(req, res, next) {
+  logger.info("进入首页......")
+  res.render('index5', { title: 'Express' });
+});
+router.get('/portal6', function(req, res, next) {
+  logger.info("进入首页......")
+  res.render('index6', { title: 'Express' });
+});
+router.get('/portal7', function(req, res, next) {
+  logger.info("进入首页......")
+  res.render('index7', { title: 'Express' });
+});
 module.exports = router;
 
